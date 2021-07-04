d9aee0d141c92b452629cdb6c5eaece9f01cd282
Zikri89 <asalngoding89@gmail.com>
2021-07-04 18:06:45 +0700

    menghilangkan menu konfirmasi pembayaran dan menu pesanan jika user tidak login

--- application/views/home/_include/navbar.php
+++ application/views/home/_include/navbar.php
@@ -135,25 +135,27 @@
                                         <?php
                                         $menu = $this->model_menu->menu_main();
                                         foreach ($menu->result_array() as $row) {
-                                            if($this->session->level){
-                                                $menu = $row['nama_menu'] ? $row['nama_menu'] : "";
-                                            }else{
-                                                $menu = $row['nama_menu']=="Konfirmasi Pembayaran" ? "" : $row['nama_menu'];
-                                            }
                                             $dropdown = $this->model_menu->dropdown_menu($row['id_menu'])->num_rows();
                                             if ($dropdown == 0) {
+                                                if($this->session->level){
+                                                    $menus = $row['nama_menu'];
+                                                }else{
+                                                    $menus = $row['nama_menu']=="Konfirmasi Pembayaran" ? "" : $row['nama_menu'];
+                                                }
                                         ?>
                                                 <li class="nav-links__item">
                                                     <a class="nav-links__item-link" href="<?= base_url() . $row['link']; ?>">
-                                                        <div class="nav-links__item-body"><?= $menu; ?></div>
+                                                        <div class="nav-links__item-body"><?= $menus; ?></div>
                                                     </a>
                                                 </li>
 
-                                            <?php } else { ?>
-
-                                                <li class="nav-links__item nav-links__item--has-submenu">
+                                            <?php } else { 
+                                                    if($this->session->level){
+                                                        $menus = $row['nama_menu'];
+                                                    ?>
+                                                        <li class="nav-links__item nav-links__item--has-submenu">
                                                     <a class="nav-links__item-link" href="#">
-                                                        <div class="nav-links__item-body"><?= $row['nama_menu']; ?>
+                                                        <div class="nav-links__item-body"><?= $menus; ?>
                                                             <svg class="nav-links__item-arrow" width="9px" height="6px">
                                                                 <use xlink:href="<?= base_url('assets/template/tema/') ?>images/sprite.svg#arrow-rounded-down-9x6">
                                                                 </use>
@@ -167,8 +169,8 @@
                                                             <ul class="menu__list">
                                                                 <?php
                                                                 $dropmenu = $this->model_menu->dropdown_menu($row['id_menu']);
-                                                                foreach ($dropmenu->result_array() as $row) { ?>
-
+                                                                foreach ($dropmenu->result_array() as $row) { 
+                                                                    ?>
                                                                     <li class="menu__item">
                                                                         <div class="menu__item-submenu-offset"></div>
                                                                         <a class="menu__item-link" href="<?= base_url() . $row['link']; ?>">
@@ -182,6 +184,11 @@
                                                         </div><!-- .menu / end -->
                                                     </div>
                                                 </li>
+                                                    <?php
+                                                    }else{
+                                                        $menus = $row['nama_menu']=="Pesanan" ? "" : $row['nama_menu'];
+                                                    }
+                                                ?>
 
                                         <?php }
                                         } ?>
