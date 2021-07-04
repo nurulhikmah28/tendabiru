2af2caecff755e8919048d3633415f77fb796f03
Zikri89 <asalngoding89@gmail.com>
2021-07-04 18:13:53 +0700

    modify where condition

--- application/models/Model_laporan.php
+++ application/models/Model_laporan.php
@@ -4,13 +4,12 @@ class Model_laporan extends CI_model
 
     function laporan()
     {
-        return $this->db->query("SELECT * FROM `tb_toko_penjualan` a WHERE proses='3' ORDER BY waktu_transaksi ASC");
+        return $this->db->query("SELECT * FROM `tb_toko_penjualan` a  ORDER BY waktu_transaksi ASC");
     }
 
     function laporan1()
     {
         $hari = date('Y-m-d');
-        $this->db->where("proses='3'");
         $this->db->where("waktu_transaksi='$hari'");
         $this->db->order_by('waktu_transaksi', 'asc');
         return $this->db->get('tb_toko_penjualan');
@@ -20,7 +19,6 @@ class Model_laporan extends CI_model
     function laporan7()
     {
         $hari = date('Y-m-d');
-        $this->db->where("proses='3'");
         $this->db->where("waktu_transaksi > DATE_SUB( '$hari' , INTERVAL 7 DAY )");
         $this->db->order_by('waktu_transaksi', 'asc');
         return $this->db->get('tb_toko_penjualan');
@@ -29,7 +27,6 @@ class Model_laporan extends CI_model
     function laporan30()
     {
         $hari = date('Y-m-d');
-        $this->db->where("proses='3'");
         $this->db->where("waktu_transaksi > DATE_SUB( '$hari' , INTERVAL 30 DAY )");
         $this->db->order_by('waktu_transaksi', 'asc');
         return $this->db->get('tb_toko_penjualan');
@@ -38,7 +35,6 @@ class Model_laporan extends CI_model
     function laporan360()
     {
         $hari = date('Y-m-d');
-        $this->db->where("proses='3'");
         $this->db->where("waktu_transaksi > DATE_SUB( '$hari' , INTERVAL 1 YEAR )");
         $this->db->order_by('waktu_transaksi', 'asc');
         return $this->db->get('tb_toko_penjualan');
